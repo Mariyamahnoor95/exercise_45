54dd76e9da43aa669e038ebcc3eabc3250af675f
Mariyamahnoor95 <mariyammahnoor@gmail.com>
2024-03-12 15:32:46 +0500

    27 done

---
+++ 26_Alien Colors #2/main.js
@@ -0,0 +1,17 @@
+"use strict";
+//version 1
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
+// version 2
+alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
---
+++ 26_Alien Colors #2/main.ts
@@ -0,0 +1,18 @@
+//version 1
+
+let alien_color = "green";
+
+if (alien_color === "green") {
+  console.log("The player just earned 5 points for shooting the alien.");
+} else {
+  console.log("The player just earned 10 points.");
+}
+
+// version 2
+alien_color = "yellow";
+
+if (alien_color === "green") {
+  console.log("The player just earned 5 points for shooting the alien.");
+} else {
+  console.log("The player just earned 10 points.");
+}
